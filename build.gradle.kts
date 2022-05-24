bf8dd6e9548198aa0c2272a7ec39da1ff8b80c4c
Lucas <lucasvalenteds@gmail.com>
2022-05-23 22:02:58 -0300

    Adding Cucumber to dependencies list

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     java
-    jacoco
 }
 
 repositories {
@@ -14,7 +11,12 @@ dependencies {
     implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
     implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
 
-    testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
+    testImplementation(platform("org.junit:junit-bom:${properties["version.junit"]}"))
+    testImplementation(platform("io.cucumber:cucumber-bom:${properties["version.cucumber"]}"))
+    testImplementation("io.cucumber", "cucumber-java")
+    testImplementation("io.cucumber", "cucumber-junit-platform-engine")
+    testImplementation("org.junit.platform", "junit-platform-suite")
+    testImplementation("org.junit.jupiter", "junit-jupiter")
 }
 
 java {
@@ -25,8 +27,6 @@ java {
 
 tasks.test {
     useJUnitPlatform()
-    testLogging {
-        events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
-    }
     finalizedBy(tasks.withType<JacocoReport>())
+    systemProperty("cucumber.publish.quiet", "true")
 }
